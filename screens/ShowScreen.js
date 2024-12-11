dc240f6a9c038535b87499da7aaf6e2832fd291b
H51M14\Student <Student@gmail.com>
2024-12-11 08:48:31 +0700

    finish 1d

---
+++ screens/ShowScreen.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import axios from 'axios';
+
+
+export default ShowScreen;
